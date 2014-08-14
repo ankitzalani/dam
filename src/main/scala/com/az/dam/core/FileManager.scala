5414f3d4b33eb1ff6e29be82f64e3639d6c56fe4
Ankit Zalani <ankit.littlegenius@gmail.com>
2014-08-14 20:46:24 +0530

    Added Skelton for FileManager.

---
+++ src/main/scala/com/az/dam/core/FileManager.scala
@@ -0,0 +1,19 @@
+package com.az.dam.core
+
+/**
+ * Created by ankit on 8/14/2014.
+ */
+class FileManager {
+
+  /*
+   * readFile method will read file passed as argument and call the callback method each time a line is read.
+   */
+  def readFile(filename: String, frequency: Long, retry: Long, callback: String => Unit): Unit = {
+    ???
+  }
+
+}
+
+
+
+
